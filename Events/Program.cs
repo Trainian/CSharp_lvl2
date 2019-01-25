80b9d40dcc868a98b9710fb524a893a611a93c93
Дмитрий Горбовский <dimon007@inbox.ru>
2019-01-25 11:27:47 +0300

    Заливка первого проекта "Events"

---
+++ Events/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Worker worker = new Worker(0, "Dima", "Home");
+            worker.WorkededHandler += (sender, s) =>
+            {
+                Console.WriteLine("Работа была завершена и на данный момент на счету: " + s.Sum + "\u20BD");
+                Console.WriteLine($"Спасибо за работу {s.FirstName} в нашей организации:'{s.Organization}'");
+            };
+            worker.Worked(worker);
+            worker.DoubleWorked(worker);
+            Console.ReadKey();
+        }
+
+    }
+
+    class Worker
+    {
+        public event EventHandler<Worker> WorkededHandler;
+        public int Sum { get; set; }
+        public string FirstName { get; set; }
+        public string Organization { get; set; }
+
+        public Worker(int sum, string firstName, string organization)
+        {
+            Sum = sum;
+            FirstName = firstName;
+            Organization = organization;
+        }
+
+        public void Worked(Worker worker)
+        {
+            Sum++;
+            WorkededHandler?.Invoke(this, worker);
+        }
+
+        public void DoubleWorked(Worker worker)
+        {
+            Sum += 2;
+            WorkededHandler?.Invoke(this, worker);
+        }
+    }
+}
