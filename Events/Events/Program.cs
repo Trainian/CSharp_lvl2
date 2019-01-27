98730ce5a8e3cb1e19a65a90a7520571473469eb
Дмитрий Горбовский <dimon007@inbox.ru>
2019-01-27 16:24:21 +0300

    Домашние задание 1

---
+++ Events/Events/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+using System.Reflection;
+
+namespace Events
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            List<Worker> list = new List<Worker>();
+            list.Add(new Worker(0, "Dima", "Home"));
+            list.Add(new Worker(20, "Dima2", "Home2"));
+            list.Add(new Worker(330, "Dima3", "Home3"));
+            SaveAsXmlFormat(list, "data.xml");
+            LoadFromXmlFormat(ref list, "data.xml");
+            foreach (Worker i in list)
+            {
+                Console.WriteLine($"Имя: {i.FirstName}, Организация: {i.Organization}, Место работы: {i.Organization}");
+            }
+            //worker.WorkededHandler += (sender, s) =>
+            //{
+            //    Console.WriteLine("Работа была завершена и на данный момент на счету: " + s.Sum + "\u20BD");
+            //    Console.WriteLine($"Спасибо за работу {s.FirstName} в нашей организации:'{s.Organization}'");
+            //};
+            //worker.Worked(worker);
+            //worker.DoubleWorked(worker);
+            //SaveAsXMLFormat(worker, "data.xml");
+            //Worker newWorker = LoadFromXMLFormat("data.xml");
+            //Console.WriteLine($"{newWorker.FirstName} {newWorker.Organization} {newWorker.Sum}");
+            Console.ReadKey();
+            Type type = new TypeDelegator(typeof(Worker));
+            Console.ReadKey();
+        }
+
+        static void SaveAsXmlFormat(List<Worker> w, string fileName)
+        {
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<Worker>));
+            Stream str = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            xmlFormat.Serialize(str, w);
+            str.Close();
+        }
+
+        static void LoadFromXmlFormat(ref List<Worker> w,string str)
+        {
+            if (w == null) throw new ArgumentNullException(nameof(w));
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<Worker>));
+            Stream stream = new FileStream(str, FileMode.Open, FileAccess.Read);
+            w = (List<Worker>)xmlFormat.Deserialize(stream);
+            stream.Close();
+        }
+    }
+
+    [Serializable]
+    public class Worker
+    {
+        public event EventHandler<Worker> WorkededHandler;
+        public int Sum { get; set; }
+        public string FirstName { get; set; }
+        public string Organization { get; set; }
+
+        public Worker()
+        {
+
+        }
+
+        public Worker(int sum, string firstName, string organization)
+        {
+            Sum = sum;
+            FirstName = firstName;
+            Organization = organization;
+        }
+
+        public void Worked(Worker worker)
+        {
+            Sum++;
+            WorkededHandler?.Invoke(this, worker);
+        }
+
+        public void DoubleWorked(Worker worker)
+        {
+            Sum += 2;
+            WorkededHandler?.Invoke(this, worker);
+        }
+    }
+}
---
+++ HomeWork/HomeWork/Game.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HomeWork
+{
+    static class Game
+    {
+        private static BufferedGraphicsContext context;
+        static public BufferedGraphics buffer;
+
+        static public int Width { get; set; }
+        static public int Height { get; private set; }
+
+        static public BaseObject[] objs;
+
+        static Game()
+        { }
+
+        static public void Init(Form form)
+        {
+            Graphics g;
+            context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics();
+            Width = form.Width;
+            Height = form.Height;
+            buffer = context.Allocate(g, new Rectangle(0, 0, Width, Height));
+            Timer timer = new Timer();
+            timer.Interval = 100;
+            timer.Start();
+            timer.Tick += Timer_Tick;
+        }
+
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+
+        static public void Draw()
+        {
+            buffer.Graphics.Clear(Color.Black);
+            foreach (BaseObject obj in objs)
+            {
+                obj.Draw();
+            }
+            buffer.Render();
+        }
+
+        static public void Update()
+        {
+            foreach (BaseObject obj in objs)
+            {
+                obj.Update();
+            }
+        }
+
+        static public void Load()
+        {
+            objs = new BaseObject[30];
+            int x = 0;
+            for (int i = 0; i < objs.Length/3; i++)
+            {
+                objs[i] = new BaseObject(new Point(600, i * 20), new Point(-i,-i), new Size(10,10));
+            }
+
+            for (int i = objs.Length/3; i < objs.Length/3*2; i++)
+            {
+                objs[i] = new Star(new Point(600, i * 20), new Point(- i, 0), new Size(5, 5));
+            }
+            for (int i = objs.Length/3*2; i < objs.Length; i++)
+            {
+                objs[i] = new Planet(new Point(600, i+x), new Point(-i,0), new Size(i,i));
+                x += 80;
+            }
+        }
+    }
+}
