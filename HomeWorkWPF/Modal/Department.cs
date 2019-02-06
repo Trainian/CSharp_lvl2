ce7ff3a2ea7616ba9e1f91a949022e724b87e18e
Дмитрий Горбовский <dimon007@inbox.ru>
2019-02-07 00:49:16 +0300

    Домашнее задание 6 (готовое)

--- HomeWorkWPF/Modal/Department.cs
+++ HomeWorkWPF/Modal/Department.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using HomeWorkWPF.Annotations;
 
 namespace HomeWorkWPF
 {
@@ -25,5 +28,6 @@ namespace HomeWorkWPF
         {
             return $"{Title}";
         }
+
     }
 }
